5bc4822f6694464f6af679fe4bfab727c1efa401
karimfarid2004 <66212191+karimfarid2004@users.noreply.github.com>
2023-11-03 22:53:29 +0200

    Update Output.h

--- GUI/Output.h
+++ GUI/Output.h
@@ -13,11 +13,13 @@ public:
 	void CreateDrawToolBar() const;	//creates Draw mode toolbar & menu
 	void CreatePlayToolBar() const;	//creates Play mode toolbar & menu
 	void CreateStatusBar() const;	//create the status bar
+	void CreateFillColorsBar() const; //ADDED BY KARIM
+
 
 	Input* CreateInput() const; //creates a pointer to the Input object	
 	void ClearStatusBar() const;	//Clears the status bar
 	void ClearDrawArea() const;	//Clears the drawing area
-	void ClearToolBar() const;
+	void ClearToolBar() const;  //ADDED BY KARIM
 	// -- Figures Drawing functions
 	void DrawRect(Point P1, Point P2, GfxInfo RectGfxInfo, bool selected=false) const;  //Draw a rectangle
 	
