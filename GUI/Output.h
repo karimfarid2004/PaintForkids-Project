95b433459999b6d36f4bf1c554f05a1686be5574
karimfarid2004 <66212191+karimfarid2004@users.noreply.github.com>
2023-11-03 23:01:48 +0200

    Update Output.h
    clear bar
    create color and filled color bar .h

--- GUI/Output.h
+++ GUI/Output.h
@@ -13,13 +13,13 @@ public:
 	void CreateDrawToolBar() const;	//creates Draw mode toolbar & menu
 	void CreatePlayToolBar() const;	//creates Play mode toolbar & menu
 	void CreateStatusBar() const;	//create the status bar
-	void CreateFillColorsBar() const; //ADDED BY KARIM
-
+	void CreateFillColorsBar() const; //ADDED BY KARIM creates tool bar for fill color
+	void CreateColorBar() const; //ADDED BY KARIM creates tool bar for color
 
 	Input* CreateInput() const; //creates a pointer to the Input object	
 	void ClearStatusBar() const;	//Clears the status bar
 	void ClearDrawArea() const;	//Clears the drawing area
-	void ClearToolBar() const;  //ADDED BY KARIM
+	void ClearToolBar() const;  //ADDED BY KARIM clears the current tool bar (use with create bars)
 	// -- Figures Drawing functions
 	void DrawRect(Point P1, Point P2, GfxInfo RectGfxInfo, bool selected=false) const;  //Draw a rectangle
 	
